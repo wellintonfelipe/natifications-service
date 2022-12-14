e0617c7a867407dc2b7db7e07f2998729a1a4631
Wellinton Felipe <wellintonfelipe@hotmail.com.br>
2022-12-14 12:05:54 -0300

    organization class in TypeScript Get e Set.

---
+++ src/application/entities/content.ts
@@ -0,0 +1,19 @@
+export class Content {
+  private readonly content: string;
+
+  get value(): string {
+    return this.content;
+  }
+
+  private validateContentLength(content: string): boolean {
+    return content.length >= 5 && content.length <= 255;
+  }
+  constructor(content: string) {
+    const isContentLenghtValid = this.validateContentLength(content);
+
+    if (!isContentLenghtValid) {
+      throw new Error('Content lenght error.');
+    }
+    this.content = content;
+  }
+}
