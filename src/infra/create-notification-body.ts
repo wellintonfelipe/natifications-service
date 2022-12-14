384ea814a85d243ba22aa865cf1fa2b04b4d0f0d
Wellinton Felipe <wellintonfelipe@hotmail.com.br>
2022-12-14 11:13:10 -0300

    organization file in directoris

---
+++ src/infra/create-notification-body.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 255)
+  content: string;
+
+  category: string;
+}
